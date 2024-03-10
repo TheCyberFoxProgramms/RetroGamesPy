96ccdd097c6dbf70c7e8bb01aca9e689f915deb1
TheCyberFox <666@gmail.com>
2024-03-10 10:14:06 +0300

    update puckman

--- puckman.py
+++ puckman.py
@@ -1,5 +1,6 @@
 import pygame
-from pprint import pprint
+from random import randint
+from GameOver import game_over_main
 
 def puckman_main():
     pygame.init()
@@ -10,7 +11,13 @@ def puckman_main():
     clock = pygame.time.Clock()
     running = True
 
+    size_font = 15
+    game_font = pygame.font.Font(r'data/Minecraft Seven_2.ttf', size_font)
+    lvl = 1
     game_map = None
+    count_item = 0
+    count_item_check = 0
+    game_exit = None
     pygame.mixer.music.load('data/sounds/pucman/pucman_main.mp3')
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.2)
@@ -59,7 +66,7 @@ def puckman_main():
         logic_mass.append(logic_mass_promt)
         tails_pos_y += 50
 
-    pprint(logic_mass)
+
 
     def start_pos_gg(mod=False):
         for index, value in enumerate(logic_mass):
@@ -123,23 +130,95 @@ def puckman_main():
     gg_sprites.add(Gg(*start_pos_gg()))
 
     class Enamy(pygame.sprite.Sprite):
-        def __init__(self, pos_x, pos_y):
+        def __init__(self, pos_x, pos_y, pos_courd):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.image.load(r'data/image/pucman/puck_enamy.png')
             self.rect = self.image.get_rect()
             self.rect.x = pos_x
             self.rect.y = pos_y
+            self.pos_courd_y, self.pos_courd_x = pos_courd
+            self.move = [self.rect.y, self.rect.x]
+            self.move_range = (1, -1)
+            self.time = 0
+
+
+        def update(self, *args, **kwargs):
+            nonlocal game_exit
+            if self.time == 0:
+                y_or_x = randint(0, 1)
+                if y_or_x == 0:
+                    simvol = self.move_range[randint(0, 1)]
+                    if logic_mass[self.pos_courd_y + simvol][self.pos_courd_x] != 1:
+                        if simvol == 1:
+                            self.rect.y += 50
+                            self.pos_courd_y += 1
+                        elif simvol == -1:
+                            self.pos_courd_y -= 1
+                            self.rect.y -= 50
+                if y_or_x == 1:
+                    simvol = self.move_range[randint(0, 1)]
+                    if logic_mass[self.pos_courd_y][self.pos_courd_x + simvol] != 1:
+                        if simvol == 1:
+                            self.rect.x += 50
+                            self.pos_courd_x += 1
+                        elif simvol == -1:
+                            self.rect.x -= 50
+                            self.pos_courd_x -= 1
+
+            self.time += 1
+            if self.time > 10:
+                self.time = 0
+            if pygame.sprite.spritecollideany(self, gg_sprites):
+                old_base = None
+                with open('data/database/RaitingDb.txt', encoding='utf-8') as f:
+                    old_base = f.read().rstrip().split(',')
+                    old_base[0] = str(int(old_base[0]) + count_item_check)
+                    old_base = ','.join(old_base)
+                with open('data/database/RaitingDb.txt', 'w', encoding='utf-8') as f:
+                    f.write(old_base)
+                status = game_over_main()
+                if status == 1:
+                    game_exit = 1
+                elif status == 2:
+                    game_exit = 2
+                elif status == 0:
+                    game_exit = 0
+
+
+    class Item(pygame.sprite.Sprite):
+        def __init__(self, pos_x, pos_y):
+            pygame.sprite.Sprite.__init__(self)
+            self.surf = pygame.Surface((10, 10))
+            self.surf.fill('green')
+            self.image = self.surf
+            self.rect = self.image.get_rect()
+            self.rect.x = pos_x + 20
+            self.rect.y = pos_y + 20
+
+        def update(self, *args, **kwargs):
+            nonlocal count_item_check
+            if pygame.sprite.spritecollideany(self, gg_sprites):
+                count_item_check += 1
+                self.kill()
+
+
 
     for index, value in enumerate(logic_mass):
         for i, v in enumerate(value):
             if v == 3:
-                enamy_sprites.add(Enamy(i * 50, index * 50))
+                enamy_sprites.add(Enamy(i * 50, index * 50, (index, i)))
+            elif v == 0:
+                item_sprites.add(Item(i * 50, index * 50))
+                count_item += 1
+
+
+
+
 
     def check_move(x):
         if x == 1:
             old_pos = start_pos_gg(mod=True)
-            print(old_pos)
-            if logic_mass[old_pos[1]][old_pos[0] - 1] == 0:
+            if logic_mass[old_pos[1]][old_pos[0] - 1] == 0 or logic_mass[old_pos[1]][old_pos[0] - 1] == 3:
                 logic_mass[old_pos[1]][old_pos[0]] = 0
                 logic_mass[old_pos[1]][old_pos[0] - 1] = 2
                 return True
@@ -147,8 +226,7 @@ def puckman_main():
                 return False
         if x == 2:
             old_pos = start_pos_gg(mod=True)
-            print(old_pos)
-            if logic_mass[old_pos[1]][old_pos[0] + 1] == 0:
+            if logic_mass[old_pos[1]][old_pos[0] + 1] == 0 or logic_mass[old_pos[1]][old_pos[0] + 1] == 3:
                 logic_mass[old_pos[1]][old_pos[0]] = 0
                 logic_mass[old_pos[1]][old_pos[0] + 1] = 2
                 return True
@@ -156,8 +234,7 @@ def puckman_main():
                 return False
         if x == 3:
             old_pos = start_pos_gg(mod=True)
-            print(old_pos)
-            if logic_mass[old_pos[1] - 1][old_pos[0]] == 0:
+            if logic_mass[old_pos[1] - 1][old_pos[0]] == 0 or logic_mass[old_pos[1] - 1][old_pos[0]] == 3:
                 logic_mass[old_pos[1]][old_pos[0]] = 0
                 logic_mass[old_pos[1] - 1][old_pos[0]] = 2
                 return True
@@ -165,8 +242,7 @@ def puckman_main():
                 return False
         if x == 4:
             old_pos = start_pos_gg(mod=True)
-            print(old_pos)
-            if logic_mass[old_pos[1] + 1][old_pos[0]] == 0:
+            if logic_mass[old_pos[1] + 1][old_pos[0]] == 0 or logic_mass[old_pos[1] + 1][old_pos[0]] == 3:
                 logic_mass[old_pos[1]][old_pos[0]] = 0
                 logic_mass[old_pos[1] + 1][old_pos[0]] = 2
                 return True
@@ -201,9 +277,27 @@ def puckman_main():
 
         screen.fill((0, 0, 0))
         tails_sprites.draw(screen)
+        item_sprites.draw(screen)
+        item_sprites.update()
         gg_sprites.draw(screen)
         gg_sprites.update()
         enamy_sprites.draw(screen)
+        enamy_sprites.update()
+
+        screen.blit(game_font.render(f'Собрано предметов: {count_item_check} из: {count_item} Уровень: {lvl}', 1, 'green'), (50, 45))
+        pygame.display.flip()
+        clock.tick(FPS)
+        if count_item_check >= count_item:
+            lvl += 1
+            print(666)
+            count_item_check = 0
+            count_item = 0
+        if game_exit == 1:
+            return 1
+        elif game_exit == 0:
+            running = False
+        elif game_exit == 2:
+            return 666
         pygame.display.flip()
         clock.tick(FPS)
     return 0
